3b2f6baa0ab646de635b3b701305b7ac7408ab7c
Carolina Vejar <caro.vejarc@gmail.com>
2023-03-11 09:43:58 -0300

    Tarea Memorize

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { CallbackHook } from './06-memos/CallbackHook'
+// import { CallbackHook } from './06-memos/CallbackHook'
+import { Padre } from './07-tarea-memo/Padre'
 // import { MemooHook } from './06-memos/MemoHook'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import { MultipleCUstonHook } from './03-example/MultipleCustomHook'
@@ -15,6 +16,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <CallbackHook />
+    <Padre />
   // </React.StrictMode>,
 )
