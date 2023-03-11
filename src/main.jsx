51ac9e44d5adae93a0d47c5a8573818a259df306
Carolina Vejar <caro.vejarc@gmail.com>
2023-03-11 20:07:40 -0300

    Continuacion de proyecto - useReducer

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import { CallbackHook } from './06-memos/CallbackHook'
 import { Padre } from './07-tarea-memo/Padre'
+import './08-useReducer/intro-reducer'
 // import { MemooHook } from './06-memos/MemoHook'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import { MultipleCUstonHook } from './03-example/MultipleCustomHook'
@@ -14,8 +15,8 @@ import { Padre } from './07-tarea-memo/Padre'
 // import { HooksApp } from './HooksApp'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <Padre />
-  // </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   // <React.StrictMode>
+//     <Padre />
+//   // </React.StrictMode>,
+// )
