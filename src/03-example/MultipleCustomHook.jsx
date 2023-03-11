62a75284e8aefe82ed7bb13647994dbafd807762
Carolina Vejar <caro.vejarc@gmail.com>
2023-03-10 23:01:08 -0300

    useFetch + useCounter

--- src/03-example/MultipleCustomHook.jsx
+++ src/03-example/MultipleCustomHook.jsx
@@ -6,10 +6,37 @@ export const MultipleCUstonHook = () => {
   
     console.log({ data, isLoading, hasError });
 
+    const { author , quote } = !!data && data[0]
+    // es como una condicional ,
+    // si la primera condicion ejecuta la siguiente
+
+    // undefined    -> undefined
+    // !undefined   -> true
+    // !!!undefined -> false
 
     return (
         <>
             <h4> Breaking Bad Quotes</h4>
+            <hr />
+
+            {
+                ( isLoading) ?
+                (
+                    <div className="alert alert-info text-center">
+                        Loading
+                    </div>
+                ) : 
+                (
+                    <blockquote className="blockquote text-end">
+                        <p className="mb-1">{author}</p>
+                        <footer className="blockquote-footer">{quote}</footer>
+
+                    </blockquote>
+                )
+            }
+             <button className="btn btn-primary">Next Quote</button>
+
+            
         </>
   )
 }
