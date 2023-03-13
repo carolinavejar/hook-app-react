414de6ba3a090fcab1b0964e9b09d10c7ae70331
Carolina Vejar <caro.vejarc@gmail.com>
2023-03-13 10:20:03 -0300

    Idea general de un reducer - via codigo

--- src/08-useReducer/intro-reducer.js
+++ src/08-useReducer/intro-reducer.js
@@ -1,1 +1,31 @@
-console.log("Hola mundo");
+const initialState = [{
+    id: 1,
+    todo: "Recolectar piedra del alma",
+    done: false 
+}];
+
+const todoReducer = ( state = initialState, action = {})=> {
+    // console.log(" to do reducer ");
+
+    if ( action.type === '[TODO] add todo' ) {
+        return [...state, action.payload]
+    }
+    return state;
+}
+
+let todos = todoReducer();
+
+const newTodo = {
+    id: 2,
+    todo: "Recolectar piedra del poder",
+    done: false 
+}
+
+const addTodoAction ={
+    type: '[TODO] add todo',
+    payload: newTodo
+}
+
+todos = todoReducer(todos, addTodoAction);
+
+console.log({state: todos});
